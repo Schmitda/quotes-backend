9f9efbb61ed7f72bf22fd4127239706bc5cbe835
Schmitda <daniel.schmitz@schmitz-dynamics.ch>
2020-05-05 21:33:55 +0200

    Database try

--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -2,11 +2,33 @@ import { Module } from '@nestjs/common';
 import { QuotesController } from './quotes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuoteEntity } from './quote.entity';
+import * as PostgressConnectionStringParser from 'pg-connection-string';
+
+
+const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL);
+
+
+console.log(connectionOptions);
 
 @Module({
   controllers: [QuotesController],
   imports: [
-    TypeOrmModule.forFeature([QuoteEntity])
-  ]
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      name: connectionOptions.application_name,
+      host: connectionOptions.host,
+      port: +connectionOptions.port,
+      username: connectionOptions.user,
+      password: connectionOptions.password,
+      database: connectionOptions.database,
+      synchronize: true,
+      entities: [QuoteEntity],
+      extra: {
+        ssl: true,
+      },
+    }),
+    TypeOrmModule.forFeature([QuoteEntity]),
+  ],
 })
-export class QuotesModule {}
+export class QuotesModule {
+}
