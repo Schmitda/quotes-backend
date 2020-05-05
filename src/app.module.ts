ff5481fbe1b7dc3cabcf2f78c4e26975efdabed1
Schmitda <daniel.schmitz@schmitz-dynamics.ch>
2020-05-05 21:16:14 +0200

    Typeorm feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuotesModule } from './quotes/quotes.module';
+import { QuoteEntity } from './quotes/quote.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       connectString: process.env.DATABASE_URL,
-      ssl: true
+      ssl: true,
+      entities: [QuoteEntity]
     }),
     QuotesModule,
   ],
--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuotesController } from './quotes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuoteEntity } from './quote.entity';
 
 @Module({
-  controllers: [QuotesController]
+  controllers: [QuotesController],
+  imports: [
+    TypeOrmModule.forFeature([QuoteEntity])
+  ]
 })
 export class QuotesModule {}
